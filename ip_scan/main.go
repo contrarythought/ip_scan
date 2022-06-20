120767148081f30d25306f97e037d48e2b61a793
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-06-20 12:41:26 -0400

    Update main.go

--- ip_scan/main.go
+++ ip_scan/main.go
@@ -4,36 +4,35 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
-	"time"
+	"sync"
 )
 
-func discover(addr string) bool {
+type dev_map struct {
+	mu     sync.Mutex
+	ip_map map[string]bool
+}
+
+func discover(addr string, device_map *dev_map) {
 	fmt.Println("Testing: ", addr)
 
 	cmd := exec.Command("ping", addr)
 	output, _ := cmd.Output()
-	fmt.Println(string(output))
 
 	if strings.Contains(string(output), "bytes=") == false {
-		return false
+		return
 	}
-	return true
+
+	device_map.ip_map[addr] = true
 }
 
 func main() {
-	//var b4 uint8
-	var discovered bool
-	var dev_map map[string]bool
-
-	// discover a device
-
-	rand.Seed(time.Now().Unix())
-	dev_map = make(map[string]bool)
+	device_map := &dev_map{
+		ip_map: make(map[string]bool),
+	}
 
 	f, err := os.Create("addresses.json")
 	if err != nil {
@@ -41,25 +40,20 @@ func main() {
 	}
 	defer f.Close()
 
+	var wg sync.WaitGroup
+
+	// ping concurrently
 	for i := 0; i < 256; i++ {
-		//mask := uint16(rand.Intn(0xFFFF))
-
-		// local addresses
-		addr := "192.168.1"
-
-		// read in least sig byte
-		//b4 = uint8(mask & 0xFF)
-
-		addr = addr + "." + strconv.Itoa(i)
-
-		discovered = discover(addr)
-
-		if discovered {
-			dev_map[addr] = true
-		}
+		wg.Add(1)
+		go func(b4 int) {
+			addr := "192.168.1" + "." + strconv.Itoa(b4)
+			discover(addr, device_map)
+			wg.Done()
+		}(i)
 	}
+	wg.Wait()
 
-	result, err := json.MarshalIndent(dev_map, "", "    ")
+	result, err := json.MarshalIndent(device_map.ip_map, "", "    ")
 	if err != nil {
 		log.Fatal("Failed to marshal: ", err)
 	}
