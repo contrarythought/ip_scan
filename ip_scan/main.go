63cc516b97059b0c2e576c025327b622829feaf6
Anthony Thorpe <59274215+contrarythought@users.noreply.github.com>
2022-06-23 23:23:43 -0400

    Update main.go

--- ip_scan/main.go
+++ ip_scan/main.go
@@ -9,10 +9,11 @@ import (
 	"strconv"
 	"strings"
 	"sync"
+	"time"
 )
 
 type dev_map struct {
-	mu     sync.Mutex
+	mu     sync.Mutex // most likely don't need because each goroutine is working with a different address
 	ip_map map[string]bool
 }
 
@@ -26,13 +27,18 @@ func discover(addr string, device_map *dev_map) {
 		return
 	}
 
+	fmt.Println("Connected: ", addr)
 	device_map.ip_map[addr] = true
 }
+
+var num_workers = 255
 
 func main() {
 	device_map := &dev_map{
 		ip_map: make(map[string]bool),
 	}
+
+	ip_chan := make(chan string)
 
 	f, err := os.Create("addresses.json")
 	if err != nil {
@@ -40,19 +46,31 @@ func main() {
 	}
 	defer f.Close()
 
-	var wg sync.WaitGroup
+	// initialize workers
+	for i := 0; i < num_workers; i++ {
+		go func(worker_id int) {
+			for ip := range ip_chan {
+				discover(ip, device_map)
+			}
+		}(i + 1)
+	}
+
+	var wg2 sync.WaitGroup
 
 	// ping concurrently
 	for i := 0; i < 256; i++ {
-		wg.Add(1)
+		wg2.Add(1)
 		go func(b4 int) {
-			addr := "192.168.1" + "." + strconv.Itoa(b4)
-			discover(addr, device_map)
-			wg.Done()
+			addr := "192.168.1." + strconv.Itoa(b4)
+			ip_chan <- addr
+			wg2.Done()
 		}(i)
 	}
-	wg.Wait()
 
+	wg2.Wait()
+	close(ip_chan)
+
+	time.Sleep(time.Second * 1)
 	result, err := json.MarshalIndent(device_map.ip_map, "", "    ")
 	if err != nil {
 		log.Fatal("Failed to marshal: ", err)
